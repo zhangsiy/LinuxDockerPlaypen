c056381510559f25d9e539a7e4ef93fe40ed29c4
Jeff Zhang <jeffzhang_misc@hotmail.com>
2017-08-09 18:58:54 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# LinuxDockerPlaypen
+# WindowsDockerPlaypen
+
+* Run `npm install`
+* See `gulp --tasks` for list of gulp tasks available
+* Dockerfile for local development should be named "Dockerfile_Dev" and sit in the package root folder
+* Dockerfile for Build Server should be named "Dockerfile_Build" and sit in the package root folder
---
+++ gulpfile.js
@@ -0,0 +1,121 @@
+//##########################################
+// TODO:
+//   * Real environment from git branch
+//   * Real version
+//   * Real storage for AWS secrets
+//   * Real tag/repo handling for build images
+//##########################################
+
+const {restore, test, build, publish, run} = require('gulp-dotnet-cli');
+const gulp = require('gulp');
+const del = require('del');
+const path = require('path');
+const spawn = require('child-process-promise').spawn;
+const argv = require('yargs').argv;
+
+const configuration = 'Release';
+const version = '1.1.1';
+const publishOutputDir = path.join(process.cwd(), 'output', 'publishOutput');
+const sourceDir = path.join(process.cwd(), 'src');
+const outputDir = path.join(process.cwd(), 'output');
+
+const dockerFileDir = path.join(process.cwd(), 'docker');
+const buildDockerFilePath = path.join(dockerFileDir, 'build', 'Dockerfile');
+const bundleDockerFilePath = path.join(dockerFileDir, 'bundle', 'Dockerfile');
+
+const environment = 'local';
+const dockerEnvironment = argv.dockerenv || environment;
+const dockerTag = argv.dockertag || `${dockerEnvironment}-${version}`;
+
+const containerPort = argv.containerport || '5000';
+const hostPort = argv.hostport || '80';
+
+// ========================= User Variables (Fill this out!) ======================================
+const mainProjectName = 'TestWebApplication';
+const registryUri = '119381170469.dkr.ecr.us-east-1.amazonaws.com/jeff-linux-container-testbed';
+// ================================================================================================
+
+const entryAssemblyName = `${mainProjectName}.dll`;
+const buildImageTag = `build:${dockerTag}`;
+const bundleImageTag = `${registryUri}:${dockerTag}`;
+
+// ========================= Task Definitions ======================================
+
+// --------------------------- Common Development Tasks -----------------------------------
+
+gulp.task('clean', () => del(['**/bin', '**/obj', 'output/*']));
+
+gulp.task('restore', ['clean'], () => 
+	gulp.src('./src/*.sln')
+		.pipe(restore())
+);
+
+gulp.task('build', ['restore'], () => 
+	gulp.src('./src/*.sln')
+		.pipe(build(
+			{
+				configuration: configuration,
+				version: version
+			}
+		))
+);
+
+gulp.task('test', ['build'], () =>
+	gulp.src('src/**/*Tests'.csproj)
+		.pipe(test(
+			{
+				configuration: configuration,
+				noBuild: true
+			}
+		))
+);
+
+gulp.task('publish', ['build'], () =>
+	gulp.src(`./src/${mainProjectName}`)
+		.pipe(publish(
+			{
+				configuration: configuration,
+				version: version,
+				output: publishOutputDir
+			}
+		))
+);
+
+gulp.task('run', [], () => {
+		process.chdir(`${publishOutputDir}`);
+		if (argv.rebuild) {
+			gulp.start('publish', () => 
+				spawn(`dotnet ${entryAssemblyName}`, [], { stdio: 'inherit' })
+			);
+		} else {
+			spawn(`dotnet ${entryAssemblyName}`, [], { stdio: 'inherit' });
+		}
+	}
+);
+
+gulp.task('preflight', ['publish']);
+
+
+// --------------------------- Docker Tasks -----------------------------------
+
+gulp.task('docker:compile-build-image', [], () =>
+	spawn('docker', ['build', '-t', buildImageTag, '-f', buildDockerFilePath, '.'], {stdio:'inherit'})
+	.then(() => spawn('docker', ['image', 'prune', '-f'], {stdio:'inherit'}))
+);
+
+gulp.task('docker:build-app', ['docker:compile-build-image'], () => 
+	spawn('docker', ['run', '-it', '--rm', '-v', `${sourceDir}:\app\src`, '-v', `${outputDir}:\app\output`, buildImageTag, 'gulp publish'], {stdio:'inherit'})
+);
+
+gulp.task('docker:compile-bundle-image', ['docker:build-app'], () =>
+	spawn('docker', ['build', '-t', bundleImageTag, '-f', bundleDockerFilePath, '--build-arg', `entryassembly=${entryAssemblyName}`, '--build-arg', 'artifactdir=.\\output\\publishOutput', '--build-arg', `containerport=${containerPort}`, '.'], {stdio:'inherit'})
+	.then(() => spawn('docker', ['image', 'prune', '-f'], {stdio:'inherit'}))
+);
+
+gulp.task('docker:run-app', ['docker:compile-bundle-image'], () =>
+	spawn('docker', ['run', '-it', '--rm', '-p', `${hostPort}:${containerPort}`, bundleImageTag], {stdio:'inherit'})
+);
+
+gulp.task('docker:clear-images', () => 
+	spawn('docker', ['image', 'prune', '-f'], {stdio:'inherit'})
+);
